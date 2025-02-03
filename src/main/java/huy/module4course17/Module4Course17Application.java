ec6477e56801230c26615eb65a7446f4b1c921fe
AnHuy <92672519+danganhuy@users.noreply.github.com>
2025-02-03 08:44:39 +0700

    Exercise 1 finished

---
+++ src/main/java/huy/module4course17/Module4Course17Application.java
@@ -0,0 +1,13 @@
+package huy.module4course17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4Course17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4Course17Application.class, args);
+    }
+
+}
---
+++ src/test/java/huy/module4course17/Module4Course17ApplicationTests.java
@@ -0,0 +1,13 @@
+package huy.module4course17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4Course17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
